0e071664f97c7d8bd4bd84690714c2ed4f45bc8b
Anushka Singh <ai.engineer.anushka@gmail.com>
2026-06-14 14:49:50 +0530

    Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel , computed_field, Field 
+from typing import Annotated , Literal
+import pickle
+
+app=FastAPI()
+best_rf=pickle.load(open("ipl_model.pkl","rb"))
+trf=pickle.load(open("ipl_transformer.pkl","rb"))
+
+class ChatRequest(BaseModel):
+    batting_team:Annotated[Literal['Sunrisers Hyderabad','Mumbai Indians', 'Royal Challengers Bangalore','Kolkata Knight Riders', 'Kings XI Punjab','Chennai Super Kings', 'Rajasthan Royals', 'Delhi Capitals'],Field(description="Select the batting team")]
+    bowling_team:Annotated[Literal['Sunrisers Hyderabad','Mumbai Indians', 'Royal Challengers Bangalore','Kolkata Knight Riders', 'Kings XI Punjab','Chennai Super Kings', 'Rajasthan Royals', 'Delhi Capitals'],Field(description="Select the bowling team")]
+    city:Annotated[str,Field(description="Enter Match city")]
+    runs_left:Annotated[int,Field(gt=0,description="Runs needed to win")]
+    ball_left:Annotated[int,Field(gt=0,description="Balls Remaining")]
+    wickets_left:Annotated[int,Field(ge=0,le=10,description="Wickets in hand")]
+    total_runs_x:Annotated[int,Field(gt=0,description="Target Score")]
+    current_score:Annotated[int,Field(gt=0,description="enter current score")]
+
+
+
+    @computed_field
+    def crr(self)->float:
+        return (self.current_score*6)/(120-self.ball_left)
+    
+    @computed_field
+    def rrr(self)->float:
+        return (self.runs_left*6)/self.ball_left
+    
+
+@app.get("/home")
+def home():
+    return {"message":"This is IPL Winner Prediction Project"}
+
+@app.post("/predict")
+def predict(data:ChatRequest):
+    features=pd.DataFrame({
+        "batting_team":[data.batting_team],
+        "bowling_team":[data.bowling_team],
+        "city":[data.city],
+        "runs_left":[data.runs_left],
+        "ball_left":[data.ball_left],
+        "wickets_left":[data.wickets_left],
+        "total_runs_x":[data.total_runs_x],
+        "current_score":[data.current_score],
+        "crr":[data.crr],
+        "rrr":[data.rrr]
+    })
+
+    transformed_data=trf.transform(features)
+    prediction=best_rf.predict_proba(transformed_data)
+    batting_win=round(prediction[0][1]*100,2)
+    bowling_win=round(prediction[0][0]*100,2)
+    return {"batting_team":data.batting_team,
+            "bowling_team":data.bowling_team,
+            "batting_win_probability":f"{batting_win}%",
+            "bowling_win_probability":f"{bowling_win}%"}
+
